eaaa7679572e5da06ef7cce97fa5aa6582777ff1
xxc7 <894043590@qq.com>
2021-09-23 15:52:50 +0800

    feat:微博用户模型

--- src/db/model/index.js
+++ src/db/model/index.js
@@ -4,7 +4,17 @@
  */
 
 const User = require('./User')
+const Blog = require('./Blog')
+
+// 一个用户拥有多个微博，使得查询博客时可以顺带查出用户
+Blog.belongsTo(User, {
+    foreignKey: 'userId'
+})
+
+// 查询用户顺带查出博客来
+// User.hasMany(Blog)
 
 module.exports = {
-    User
+    User,
+    Blog
 }
