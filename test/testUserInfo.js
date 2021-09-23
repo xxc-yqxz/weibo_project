b3ecbb4f6d1dd3404b0b7045dd2fbbd36669b11a
xxc7 <894043590@qq.com>
2021-09-23 20:07:27 +0800

    test:创建微博 单元测试

---
+++ test/testUserInfo.js
@@ -0,0 +1,12 @@
+/**
+ * @description 单元测试的用户信息
+ * @author xxc
+ */
+
+/**
+ *  特别提醒：cookie是用户的敏感信息，此处只能是**测试**用户的cookie
+ *  每次测试用户重新登录，都需要更新这里的cookie
+ */
+module.exports = {
+    COOKIE: 'weibo.sid=q3oFNp8avUsEHyv0g6YTeW218mRIaLG1; weibo.sid.sig=OOZf5p_j5XysOOM20dwAsXip-LI'
+}
