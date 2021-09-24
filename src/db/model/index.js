c261e34e7df98d068d355a56b7f2c60727cc9c38
xxc7 <894043590@qq.com>
2021-09-24 17:09:18 +0800

    feat:粉丝 数据模型

--- src/db/model/index.js
+++ src/db/model/index.js
@@ -5,6 +5,7 @@
 
 const User = require('./User')
 const Blog = require('./Blog')
+const UserRelation = require('./UserRelation')
 
 // 一个用户拥有多个微博，使得查询博客时可以顺带查出用户
 Blog.belongsTo(User, {
@@ -14,7 +15,17 @@ Blog.belongsTo(User, {
 // 查询用户顺带查出博客来
 // User.hasMany(Blog)
 
+// 下面两个方法创建了两个外键关系,UserRelation中的followerId、userId与User表中的id（主键绑定了关联）
+UserRelation.belongsTo(User, {
+    foreignKey: 'followerId'
+})
+
+User.hasMany(UserRelation, {
+    foreignKey: 'userId'
+})
+
 module.exports = {
     User,
-    Blog
+    Blog,
+    UserRelation
 }
